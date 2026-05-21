6a9ce6afbbbb88c433863a0949a9bdab1bbfd2eb
angbaebultti <hjh99224@gmail.com>
2026-05-21 14:06:04 +0900

    14:06

--- src/components/intro/IntroSequence.tsx
+++ src/components/intro/IntroSequence.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import ErrorScreen from './ErrorScreen'
 import GlassTunnel from './GlassTunnel'
 
@@ -11,10 +11,19 @@ export default function IntroSequence() {
     document.documentElement.style.setProperty('--control-room-opacity', '0')
   }, [])
 
+  const goToTunnel = useCallback(() => {
+    setPhase('tunnel')
+  }, [])
+
   return (
     <>
       {phase === 'error' && (
-        <ErrorScreen durationMs={3000} breakDurationMs={3000} onComplete={() => setPhase('tunnel')} />
+        <>
+          <ErrorScreen durationMs={3000} breakDurationMs={3000} onComplete={goToTunnel} />
+          <button className="intro-skip-button" type="button" onClick={goToTunnel} aria-label="Skip intro">
+            SKIP
+          </button>
+        </>
       )}
       {phase === 'tunnel' && <GlassTunnel />}
     </>
